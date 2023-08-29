2b264e6f73f6d709a66a95baf9a4c94ffb62c70e
kimsky <haneul9513@naver.com>
2023-08-29 16:01:01 +0900

    BattleScene.cs 파일생성, 전투화면 적 정보, 플레이어 정보 출력, 선택지 입력 구현

---
+++ BasicTeamProject/BasicTeamProject/src/Scene/BattleScene.cs
@@ -0,0 +1,43 @@
+﻿using BasicTeamProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTeamProject.Scene
+{
+    public class BattleScene : Scene
+    {
+        protected override void SetFunctionList()
+        {
+            _FunctionList.Add("MainScene");
+            _FunctionList.Add("MainScene");
+        }
+
+        protected override void PreOperate()
+        {
+            base.PreOperate();
+            if (_dataManager.Monsters == null)
+            {
+                _dataManager.CreateDungeon(1);
+            }
+        }
+
+        protected override void WriteView()
+        {
+            Console.WriteLine("전투 시작");
+            foreach (var monster in _dataManager.Monsters)
+            {
+                Console.WriteLine($"레벨: {monster.Level} 이름: {monster.NameID} 체력: {monster.MaxHp}");
+            }
+
+            Console.WriteLine("[플레이어 정보]");
+            Console.WriteLine($"레벨: {_dataManager.Player.Level} 이름: {_dataManager.Player.NameID}" +
+                $"({_dataManager.Player.job}) 체력: {_dataManager.Player.CurrentHP}");
+
+            Console.WriteLine("1. 공격");
+            Console.WriteLine("0. 나가기");
+        }
+    }
+}
