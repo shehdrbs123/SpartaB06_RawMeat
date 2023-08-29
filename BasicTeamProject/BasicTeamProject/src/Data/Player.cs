23040d9412a889b1c9d2754f5cb56bdb117cd0ff
shehdrbs123 <shehdrbs123@naver.com>
2023-08-29 11:55:55 +0900

    Update : 직업 선택화면 완성, 테스트는 어느정도 필요
    Scene 내에 FunctionList 이름을 수정함. _FunctionList로 그래서 주의 필요

--- BasicTeamProject/BasicTeamProject/src/Data/Player.cs
+++ BasicTeamProject/BasicTeamProject/src/Data/Player.cs
@@ -12,15 +12,17 @@ public class Player
     }
     public string NameID { get; set; }
     public Job job { get; set; }
-    public int Level { get; set; }
+    public int Level { get; set; } = 1;
+    public int CurrentHP { get; set; } = 0;
+    public int CurrentMP { get; set; } = 0;
+    public int CurrentExp { get; set; } = 0;
     public int MaxHp { get; set; }
     public int MaxMp { get; set; }
+    public int MaxExp { get; set; }
     public float Att { get; set; }
     public float Def { get; set; }
     public float Critical { get; set; }
     public float Dodge { get; set; }
-    public int Exp { get; set; }
     public List<Skill> Skills { get; set; } = new List<Skill>();
-
-    public int Gold { get; set; }
+    public int Gold { get; set; } = 1500;
 }
