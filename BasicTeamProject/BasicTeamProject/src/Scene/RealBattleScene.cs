1f352412e249cd83fb56b74a216f1c78ce3b02a2
kimsky <haneul9513@naver.com>
2023-08-31 10:50:45 +0900

    +전투 로그 출력 대기시간 증가

--- BasicTeamProject/BasicTeamProject/src/Scene/RealBattleScene.cs
+++ BasicTeamProject/BasicTeamProject/src/Scene/RealBattleScene.cs
@@ -14,7 +14,7 @@ namespace BasicTeamProject.Scene
 
         protected override void SetFunctionList()
         {
-            _FunctionList.Add("BattleSelectScene");
+            _FunctionList.Add("BattleScene");
         }
 
         protected override void PreOperate()
@@ -23,7 +23,7 @@ namespace BasicTeamProject.Scene
             preInputNum = BattleSelectScene.selectedMonster;
             monster = _dataManager.Monsters[preInputNum - 1];
 
-            monster.CurrentHP -= (int)_dataManager.Player.Att;
+            monster.CurrentHP -= (int)_dataManager.Player.GetDamage();
             if (monster.CurrentHP > 0) 
             {
                 _dataManager.Player.CurrentHP -= (int)monster.Att;
@@ -37,28 +37,30 @@ namespace BasicTeamProject.Scene
             enter();
 
             Console.WriteLine($"{_dataManager.Player.NameID}의 공격");
-            Thread.Sleep(300);
-            Console.WriteLine($"Lv.{monster.Level} {monster.NameID}을(를) 맞췄습니다. [대미지 : {(int)_dataManager.Player.Att}]");
-            Thread.Sleep(300);
+            Thread.Sleep(600);
+            Console.WriteLine($"Lv.{monster.Level} {monster.NameID}을(를) 맞췄습니다. " +
+                $"[대미지 : {(int)_dataManager.Player.GetDamage()}]");
+            Thread.Sleep(600);
 
             enter();
             Console.WriteLine($"Lv.{monster.Level} {monster.NameID}");
             Console.WriteLine($"HP {monster.CurrentHP}");
-            Thread.Sleep(300);
+            Thread.Sleep(600);
 
             enter();
             if (monster.CurrentHP > 0)
             {
                 Console.WriteLine($"{monster.NameID}의 공격");
-                Thread.Sleep(300);
+                Thread.Sleep(600);
                 Console.WriteLine($"[받은 대미지 : {monster.Att}]");
-                Thread.Sleep(300);
+                Thread.Sleep(600);
             }
 
             enter();
-            Console.WriteLine($"Lv.{_dataManager.Player.Level} {_dataManager.Player.NameID}");
-            Console.WriteLine($"HP {_dataManager.Player.CurrentHP}");
-            Thread.Sleep(300);
+            Console.WriteLine($"Lv.{_dataManager.Player.Level} {_dataManager.Player.NameID} ({_dataManager.Player.job})");
+            Console.WriteLine($"HP {_dataManager.Player.CurrentHP}/{_dataManager.Player.MaxHP}");
+            Console.WriteLine($"MP {_dataManager.Player.CurrentMP}/{_dataManager.Player.MaxMP}");
+            Thread.Sleep(600);
 
             enter();
             Console.WriteLine("0. 다음");
@@ -74,6 +76,9 @@ namespace BasicTeamProject.Scene
                 _dataManager.Monsters.Remove(monster);
                 _dataManager.Monsters.Add(temp);
             }
+            _dataManager.Player.TurnCheck();
+            _dataManager.InputMemory.InputComplete = true;
+            _dataManager.InputMemory.PreInput = 1;
         }
     }
 }
